4bc8e61f0693de1232958bc40d426247abb1f2a1
Sarthak Sharma <SharmaSarthak999@gmail.com>
2024-02-18 14:48:53 +0530

    fix(re_init) - Modified pygooglenews to remove ScraperBee and Proxies
    Removed the scraping bee and proxies functions and calls.
    Removed scraping bee and proxies params in other functions.

--- backend/pygooglenews.py
+++ backend/pygooglenews.py
@@ -42,42 +42,16 @@ class GoogleNews:
                 entries[i]['sub_articles'] = None
         return entries
 
-    def __scaping_bee_request(self, api_key, url):
-        response = requests.get(
-            url="https://app.scrapingbee.com/api/v1/",
-            params={
-                "api_key": api_key,
-                "url": url,
-                "render_js": "false"
-            }
-        )
-        if response.status_code == 200:
-            return response
-        if response.status_code != 200:
-            raise Exception("ScrapingBee status_code: "  + str(response.status_code) + " " + response.text)
+    def __parse_feed(self, feed_url):
 
-    def __parse_feed(self, feed_url, proxies=None, scraping_bee = None):
-
-        if scraping_bee and proxies:
-            raise Exception("Pick either ScrapingBee or proxies. Not both!")
-
-        if proxies:
-            r = requests.get(feed_url, proxies = proxies)
-        else:
-            r = requests.get(feed_url)
-
-        if scraping_bee:
-            r = self.__scaping_bee_request(url = feed_url, api_key = scraping_bee)
-        else:
-            r = requests.get(feed_url)
-
+        r = requests.get(feed_url)
 
         if 'https://news.google.com/rss/unsupported' in r.url:
             raise Exception('This feed is not available')
 
         d = feedparser.parse(r.text)
 
-        if not scraping_bee and not proxies and len(d['entries']) == 0:
+        if len(d['entries']) == 0:
             d = feedparser.parse(feed_url)
 
         return dict((k, d[k]) for k in ('feed', 'entries'))
@@ -94,22 +68,22 @@ class GoogleNews:
 
 
 
-    def top_news(self, proxies=None, scraping_bee = None):
+    def top_news(self):
         """Return a list of all articles from the main page of Google News
         given a country and a language"""
-        d = self.__parse_feed(self.BASE_URL + self.__ceid(), proxies=proxies, scraping_bee=scraping_bee)
+        d = self.__parse_feed(self.BASE_URL + self.__ceid())
         d['entries'] = self.__add_sub_articles(d['entries'])
         return d
 
-    def topic_headlines(self, topic: str, proxies=None, scraping_bee=None):
+    def topic_headlines(self, topic: str):
         """Return a list of all articles from the topic page of Google News
         given a country and a language"""
         #topic = topic.upper()
         if topic.upper() in ['WORLD', 'NATION', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SCIENCE', 'SPORTS', 'HEALTH']:
-            d = self.__parse_feed(self.BASE_URL + '/headlines/section/topic/{}'.format(topic.upper()) + self.__ceid(), proxies = proxies, scraping_bee=scraping_bee)
+            d = self.__parse_feed(self.BASE_URL + '/headlines/section/topic/{}'.format(topic.upper()) + self.__ceid())
 
         else:
-            d = self.__parse_feed(self.BASE_URL + '/topics/{}'.format(topic) + self.__ceid(), proxies = proxies, scraping_bee=scraping_bee)
+            d = self.__parse_feed(self.BASE_URL + '/topics/{}'.format(topic) + self.__ceid())
 
         d['entries'] = self.__add_sub_articles(d['entries'])
         if len(d['entries']) > 0:
@@ -117,15 +91,15 @@ class GoogleNews:
         else:
             raise Exception('unsupported topic')
 
-    def geo_headlines(self, geo: str, proxies=None, scraping_bee=None):
+    def geo_headlines(self, geo: str):
         """Return a list of all articles about a specific geolocation
         given a country and a language"""
-        d = self.__parse_feed(self.BASE_URL + '/headlines/section/geo/{}'.format(geo) + self.__ceid(), proxies = proxies, scraping_bee=scraping_bee)
+        d = self.__parse_feed(self.BASE_URL + '/headlines/section/geo/{}'.format(geo) + self.__ceid())
 
         d['entries'] = self.__add_sub_articles(d['entries'])
         return d
 
-    def search(self, query: str, helper = True, when = None, from_ = None, to_ = None, proxies=None, scraping_bee=None):
+    def search(self, query: str, helper = True, when = None, from_ = None, to_ = None):
         """
         Return a list of all articles given a full-text search parameter,
         a country and a language
@@ -151,7 +125,7 @@ class GoogleNews:
         search_ceid = self.__ceid()
         search_ceid = search_ceid.replace('?', '&')
 
-        d = self.__parse_feed(self.BASE_URL + '/search?q={}'.format(query) + search_ceid, proxies = proxies, scraping_bee=scraping_bee)
+        d = self.__parse_feed(self.BASE_URL + '/search?q={}'.format(query) + search_ceid)
 
         d['entries'] = self.__add_sub_articles(d['entries'])
         return d
