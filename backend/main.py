9dd726f140b761ad4f9069ca85114ca95047a566
Sarthak Sharma <SharmaSarthak999@gmail.com>
2024-02-18 20:04:54 +0530

    Added GPT support Now fetching all enteries and generating a summary based on them. The summary generated is based on 5W and 1H method.

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,9 @@ import requests
 import json
 from pygooglenews import GoogleNews
 from newspaper import Article
+from gpt import generate_script
 
-gn = GoogleNews(lang='en', country='IN')
+
 
 def redirect_link(link):
     r = requests.get(link)
@@ -28,11 +29,22 @@ def get_event_article_list(entry):
         article_list.append(get_url_article_dict(article_link['url']))
     return article_list
 
-top = gn.top_news()
+gn = GoogleNews(lang='en', country='IN')
+top_news = gn.top_news() # World news, etc
 
-event_article_list = get_event_article_list(top['entries'][0])
+for entry in top_news['entries']:
+    event_article_list = get_event_article_list(entry)
 
-gpt_token_list = [{'title': d['title'], 'article': d['article']} for d in event_article_list]
-gpt_token_json = json.dumps(gpt_token_list)
+    gpt_token_list = [{'title': d['title'], 'article': d['article']} for d in event_article_list]
+    gpt_token_json = json.dumps(gpt_token_list)
 
-print(f"Event json:\n{gpt_token_json}\nlength:{len(gpt_token_json)}")
+    print(f"Token length:{len(gpt_token_json)}")
+
+    response = generate_script(gpt_token_json, 'g4f')
+    print(response)
+
+ # multiple prompt for summary generation | combine multiple outputs, generate script(video fetching sources ko combine kaise krna h): videos generated on the basis of template created
+ # references - sources for claiming genuinity
+ # DB design
+ # embeddings - gpt->number(embedding{data correlation}: can be loaded, for initial context & classification) | Eg - search via embeddings corellation
+ # graphQL - expose as a service
