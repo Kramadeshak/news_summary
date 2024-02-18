f63dc252c8db60e2b474835270de5e082e304177
Sarthak Sharma <SharmaSarthak999@gmail.com>
2024-02-18 15:43:57 +0530

    Added title in article dict. New list variable gpt_token_list now takes only title and article to reduce token size.

--- backend/main.py
+++ backend/main.py
@@ -13,13 +13,13 @@ def get_article(url):
     article = Article(url)
     article.download()
     article.parse()
-    return article.text
+    return article.title, article.text
 
 def get_url_article_dict(link):
     url_article_dict = {}
     url = redirect_link(link)
     url_article_dict['url'] = url
-    url_article_dict['article'] = get_article(url)
+    url_article_dict['title'], url_article_dict['article'] = get_article(url)
     return url_article_dict
 
 def get_event_article_list(entry):
@@ -32,6 +32,7 @@ top = gn.top_news()
 
 event_article_list = get_event_article_list(top['entries'][0])
 
-event_json = json.dumps(event_article_list)
+gpt_token_list = [{'title': d['title'], 'article': d['article']} for d in event_article_list]
+gpt_token_json = json.dumps(gpt_token_list)
 
-print(f"Event json:\n{event_json}")
+print(f"Event json:\n{gpt_token_json}\nlength:{len(gpt_token_json)}")
